2f52fe5b7e1125d748a421b0e348f427e2b2f769
Sushank Gautam <sushankg4@gmail.com>
2024-06-07 22:37:04 +0530

    updating codes

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const {MONGODB_URI} = process.env
+const connectDB = async () => {
+    try {
+        const connectInstance = await mongoose.connect(`${MONGODB_URI}/${DB_NAME}`)
+        console.log(`Mongo Db connected sucessfully ! DB HOST : ${connectInstance.connection.host}`)
+    } catch (error) {
+        console.log("Error", error);
+        throw error
+    }
+}
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
-import mongoose, { mongo } from "mongoose";
-import { DB_NAME } from "./constants";
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({path : "./env"})
 
 
-
-
-
-
-
-/*
-import express from 'express'
-const app = express()
-
-( async ()=>{
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("Error", (error)=>{
-            console.log("Error",error)
-        })
-        
-        app.listen(process.env.PORT , ()=>{
-            console.log(`App is running on Port ${process.env.PORT}`)
-        })
-    } catch (error) {
-        console.log("ERROR : ",error)
-    } 
-})()
-*/
+connectDB()
