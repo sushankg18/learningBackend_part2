b70aa8d05eb1a6ecb1b0c2f00c8987e9cf4ec4fc
Sushank Gautam <sushankg4@gmail.com>
2024-06-10 15:17:18 +0530

    ADDED

---
+++ src/models/user.models.js
@@ -0,0 +1,89 @@
+import mongoose from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+const userSchema = mongoose.Schema(
+    {
+        userName: {
+            type: String,
+            reqiured: true,
+            unique: true,
+            lowercase: true,
+            trim: true, //trim is used for removing the spaces
+            index: true //index is used for searching
+        },
+        fullname: {
+            type: String,
+            required: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        avatar: {
+            type: String, //from cloudinary url
+            required: true
+        },
+        coverImage: {
+            type: String, //from cloudinary url
+        },
+        watchHistory: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: true,
+        },
+        refreshTokens: {
+            type: String
+        }
+    }
+    , { timestamps: true }
+)
+
+// we are using pre hooks to hash the password before saving it to the database
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) { // this.isModified is used to check if the password is modified or not
+        this.password = bcrypt.hash(this.password, 10) //if the password is modified then we are hashing it using bcrypt
+        next();
+    } else {
+        return next(); //if the password is not modified then we are returning the next function
+    }
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) { //this is a method which is used to check if the password is correct or not
+    return await bcrypt.compare(password, this.password) //we are comparing the password with the hashed password
+}
+
+userSchema.methods.generateAccessToken = function () {
+    jwt.sign({
+        _id: this._id,
+        userName: this.userName,
+        email: this.email,
+        fullname: this.fullname
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign({
+        _id: this._id,
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+export const User = mongoose.model("User", userSchema)
---
+++ src/models/video.models.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile :{
+            type : String, //from cloudinary url
+            required : true
+        },
+        thumbnail : {
+            type : String, //from cloudinary url
+            required : true,
+        },
+        title : {
+            type : String,
+            required : true,
+        },
+        description : {
+            type : String,
+        },
+        duration : {
+            type : Number,
+            required : true,
+        },
+        views : {
+            type : Number,
+            default : 0
+        },
+        isPublished : {
+            type : Boolean,
+            required :true,
+        },
+        owner : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        }
+    }
+    ,{timestamps : true}
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video", videoSchema)
